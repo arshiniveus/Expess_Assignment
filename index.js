586eee10b70cd20873de0fb475664a9a44d493a5
arshiyaniveus <arshiya.u@niveussolutions.com>
2023-08-23 20:11:52 +0530

    postman Added

--- index.js
+++ index.js
@@ -8,48 +8,52 @@ const url = path.join(__dirname, filename)
 
 console.log(url);
 app.use(express.json());
-app.use(bodyparser.urlencoded({ extended: true }))
+app.use(bodyparser.urlencoded({ extended: true }));
 
-
-http://localhost:8080/user/100/add
-
+// GET with Path Parameters
 app.get('/user/:id/:val', (req, res) => {
-
-    let myObject = {
-        id: req.params.id,
-        val: req.params.val
-
+    try {
+        let myObject = {
+            id: req.params.id,
+            val: req.params.val
+        }
+        res.send(`<h1>${JSON.stringify(myObject)}</h1>`);
+        console.log(myObject);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('An error occurred');
     }
-    res.send(`<h1>${JSON.stringify(myObject)}</h1>`);
-    console.log(myObject);
+});
 
-})
-
-http://localhost:8080/user?id=100&val=add  Path Params
-
+// GET with Query Parameters
 app.get('/user', (req, res) => {
-
-    let myObject2 = {
-        id: req.query.id,
-        val: req.query.val
-
+    try {
+        let myObject2 = {
+            id: req.query.id,
+            val: req.query.val
+        }
+        res.send(`<h1>${JSON.stringify(myObject2)}</h1>`);
+        console.log(myObject2);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('An error occurred');
     }
-    res.send(`<h1>${JSON.stringify(myObject2)}</h1>`);
-    console.log(myObject2);
+});
 
-})
-
-
-//POST api which acccepts a JSON data. Return an array of data.
+// POST API which accepts JSON data and returns an array of data.
 app.post('/', (req, res) => {
-    console.log(`JSON Data:` + JSON.stringify(req.body));
+    try {
+        console.log(`JSON Data:` + JSON.stringify(req.body));
 
-    const result = [ { data: req.body, status: 200, message: 'data fetched successfully' } ];
-    console.log(result)
-    res.send(result)
-})
+        const result = [ { data: req.body, status: 200, message: 'data fetched successfully' } ];
+        console.log(result);
+        res.send(result);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('An error occurred');
+    }
+});
 
 app.listen(port, () => {
-    console.log(`ready to listn ${port}`);
-})
-
+    console.log(`ready to listen on ${port}`);
+});
